496dcb6977ef10571c8c413a5191f38bd55e4848
alawa2020 <huder13168@gmail.com>
2021-12-22 17:06:55 -0500

    🧍 authReducer inside context

--- src/AppReducer.js
+++ src/AppReducer.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useReducer, useState } from 'react';
+import authReducer from './auth/authReducer';
 import TodosScreen from './components/todos/TodosScreen';
 import NavBar from './components/ui/NavBar';
 import TodoContext from './contexts/TodoContext';
@@ -7,6 +8,7 @@ import todoReducer from './reducers/todoReducer';
 import AppRouter from './routers/AppRouter';
 
 const AppReducer = () => {
+  // useReducer of Todos
   const init = () => {
     return JSON.parse(localStorage.getItem('list-todos-best')) || [];
   };
@@ -23,10 +25,27 @@ const AppReducer = () => {
     localStorage.setItem('list-todos-best', JSON.stringify(todos));
   }, [todos]);
 
+  // userReducer of auth
+  const initAuth = () => {
+    return localStorage.getItem('user-auth-todos') || { logged: false };
+  };
+  const [userAuth, dispatchAuth] = useReducer(authReducer, {}, initAuth);
+
+  useEffect(() => {
+    localStorage.setItem('user-auth-todos', JSON.stringify(userAuth));
+  }, [userAuth]);
+
   return (
     <div>
       <TodoContext.Provider
-        value={{ todos, dispatch, editionMode, setEditionMode }}
+        value={{
+          todos,
+          dispatch,
+          editionMode,
+          setEditionMode,
+          userAuth,
+          dispatchAuth,
+        }}
       >
         <AppRouter />
       </TodoContext.Provider>
